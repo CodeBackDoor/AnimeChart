116ed29262998acee8487d2aba93ceb98dd2682c
Moon Antonio <pinchodev91@gmail.com>
2018-08-29 16:57:27 +0200

    Data persistente

---
+++ AnimeChart Source/Assets/AnimeChart/Scripts/Clases/AnimeDB.cs
@@ -0,0 +1,31 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// AnimeDB.cs (29/08/2018)														\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Data contenedora de los animes.								\\
+// Fecha Mod:		29/08/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using System.Collections.Generic;
+using MoonAntonio.Clases;
+#endregion
+
+namespace MoonAntonio.Data
+{
+	/// <summary>
+	/// <para>Data contenedora de los animes.</para>
+	/// </summary>
+	[CreateAssetMenu(fileName = "AnimeDB", menuName = "Create/AnimeDB", order = 1)]
+	public class AnimeDB : ScriptableObject 
+	{
+		#region Variables Publicas
+		public string nombreObjeto = "New AnimeDB";
+		public List<AnimeTv> listaAnimeTV = new List<AnimeTv>();
+		public List<AnimeOVA> listaAnimeOVA = new List<AnimeOVA>();
+		public List<AnimeMovie> listaAnimeMovie = new List<AnimeMovie>();
+		#endregion
+	}
+}
---
+++ AnimeChart Source/Assets/AnimeChart/Scripts/Clases/AnimeEnums.cs
@@ -0,0 +1,22 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// AnimeEnums.cs (29/08/2018)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Data contenedora de los animes.								\\
+// Fecha Mod:		29/08/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+#endregion
+
+namespace MoonAntonio
+{
+	public enum Temporada
+	{
+		Summer2018,
+		Autumn2018,
+		Winter2018
+	}
+}
