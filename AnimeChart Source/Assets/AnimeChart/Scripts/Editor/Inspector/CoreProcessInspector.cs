2bc56d32c4f82eb985be05fe747fee35027ab8e0
Moon Antonio <pinchodev91@gmail.com>
2018-08-29 20:38:46 +0200

    Inspector del Core

---
+++ AnimeChart Source/Assets/AnimeChart/Scripts/Editor/Inspector/CoreProcessInspector.cs
@@ -0,0 +1,72 @@
+﻿//                                  ┌∩┐(◣_◢)┌∩┐
+//																				\\
+// CoreProcessInspector.cs (29/08/2018)											\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Inspector de CoreProcess.									\\
+// Fecha Mod:		29/08/2018													\\
+// Ultima Mod:		Version Inicial												\\
+//******************************************************************************\\
+
+#region Librerias
+using UnityEngine;
+using UnityEditor;
+using MoonAntonio.Sistemas;
+using MoonAntonio.Data;
+using System;
+#endregion
+
+namespace MoonAntonio
+{
+	/// <summary>
+	/// <para>Inspector de CoreProcess.</para>
+	/// </summary>
+	[CustomEditor(typeof(CoreProcess))]
+	public class CoreProcessInspector : Editor
+	{
+		private int tab;
+		SerializedProperty dbProperty;
+
+		void OnEnable()
+		{
+			dbProperty = serializedObject.FindProperty("db");
+		}
+
+		public override void OnInspectorGUI()
+		{
+			serializedObject.Update();
+
+			CoreProcess core = (CoreProcess)target;
+
+			tab = GUILayout.Toolbar(tab, new string[] { "Produccion", "Setup", "Ayuda" });
+			switch (tab)
+			{
+				case 0:
+					EditorGUI.indentLevel++;
+
+					EditorGUILayout.PropertyField(dbProperty);
+
+					EditorGUI.indentLevel--;
+
+					if (GUILayout.Button("Generar Chart"))
+					{
+						core.GenerarChart();
+					}
+
+					if (GUILayout.Button("Crear Foto"))
+					{
+						core.CrearFoto();
+					}
+					break;
+
+				case 1:
+					break;
+
+				case 2:
+					break;
+			}
+
+			serializedObject.ApplyModifiedProperties();
+		}
+
+	}
+}
--- AnimeChart Source/Assets/AnimeChart/Scripts/Sistemas/CoreProcess.cs
+++ AnimeChart Source/Assets/AnimeChart/Scripts/Sistemas/CoreProcess.cs
@@ -1,20 +1,31 @@
 ﻿//                                  ┌∩┐(◣_◢)┌∩┐
 //																				\\
-// CoreProcess.cs (00/00/0000)													\\
-// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com								        \\
-// Descripcion:																	\\
-// Fecha Mod:		00/00/0000													\\
-// Ultima Mod:																	\\
+// CoreProcess.cs (29/08/2018)													\\
+// Autor: Antonio Mateo (.\Moon Antonio) 	antoniomt.moon@gmail.com			\\
+// Descripcion:		Sistema que genera la imagen a partir de la camara.			\\
+// Fecha Mod:		29/08/2018													\\
+// Ultima Mod:		Version Inicial												\\
 //******************************************************************************\\
 
 #region Librerias
 using UnityEngine;
+using MoonAntonio.Data;
 #endregion
 
-namespace MoonAntonio
+namespace MoonAntonio.Sistemas
 {
+	/// <summary>
+	/// <para>Sistema que genera la imagen a partir de la camara.</para>
+	/// </summary>
 	public class CoreProcess : MonoBehaviour 
 	{
+		#region Variables Publicas
+		public AnimeDB db;
+		#endregion
 
+		#region API
+		public void GenerarChart() { }
+		public void CrearFoto() { }
+		#endregion
 	}
 }
